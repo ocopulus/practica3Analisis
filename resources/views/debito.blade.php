97a603f5023b02a1211ceb0689a6cfe186ee546e
Luis Azurdia <luigitercero>
2018-04-11 19:23:50 +0200

    "corrigiendo vistas credito transaccion debito"

--- resources/views/debito.blade.php
+++ resources/views/debito.blade.php
@@ -2,25 +2,26 @@
 
 @section('cuerpo')
 <div class="card">
-		<div class="card-content ">
-		  <span class="card-title">agregar credito</span>
-		  
-		  <div class="row">
-				<div class="input-field col s12">
-				  <input id="Description" type="text" data-length="45">
-				  <label for="Description">Descripcion</label>
-				</div>
-				<div class="input-field col s12">
-						<input id="tipo" type="text" data-length="45">
-						<label for="tipo">tipo</label>
-				</div>
-				<div class="input-field col s12">
-						<input id="Monto" type="text" data-length="45">
-						<label for="Monto">Monto 0.00</label>
-				</div>
-			<div class="card-action">
-		  <a href="#" class = "btn btn-danger" >transferir</a>
-		  
+	<div class="card-content ">
+	  <span class="card-title">agregar Debito</span>
+		<div class="row">
+			<div class="input-field col s12">
+				<input id="No. Cuenta" type="number" class="validate" data-length="45">
+				<label for="No. Cuenta">Cuenta</label>
+			</div>
+			<div class="input-field col s12">
+				<input id="Description" type="text" class="validate" data-length="45">
+				<label for="Description">Descripcion</label>
+			</div>
+
+			<div class="input-field col s12">
+				<input id="Monto" type="number" data-length="45">
+				<label for="Monto">Monto 0.00</label>
+			</div>
 		</div>
+		<div class="card-action">		  
+		</div>
+	<a href="#" class = "btn btn-danger" >agregar</a>
+	</div>	
 </div>
 @endsection
--- resources/views/transferencia.blade.php
+++ resources/views/transferencia.blade.php
@@ -4,24 +4,22 @@
 
 <div class="card">
 		<div class="card-content ">
-		  <span class="card-title">agregar credito</span>
+		  <span class="card-title">Transferir</span>
 		  
 		  <div class="row">
+					
 				<div class="input-field col s12">
-				  <input id="Description" type="text" data-length="45">
-				  <label for="Description">Descripcion</label>
+					<input id="cuenta" type="number" class="validate">
+					<label for="cuenta">se dirige al numero de cuenta de</label>
 				</div>
+
 				<div class="input-field col s12">
-						<input id="tipo" type="text" data-length="45">
-						<label for="tipo">tipo</label>
+					<input id="Monto" type="number" data-length="45">
+						<label for="Monto">0.00</label>
 				</div>
-				<div class="input-field col s12">
-						<input id="Monto" type="text" data-length="45">
-						<label for="Monto">Monto 0.00</label>
-				</div>
-			<div class="card-action">
-		  <a href="#" class = "btn btn-danger" >transferir</a>
-		  
+	
+				<div class="card-action">
 		</div>
+			<a href="#" class = "btn btn-danger" >transferir</a>
 </div>
 @endsection
